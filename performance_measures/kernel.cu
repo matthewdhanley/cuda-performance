2da8b6b3d5c18c56d30a624bdc3c559101bebdf7
Matthew Hanley <mattdhanley@gmail.com>
2018-09-24 20:49:39 -0600

    finished add example and bandwidth

--- performance_measures/kernel.cu
+++ performance_measures/kernel.cu
@@ -3,6 +3,8 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 #include <stdlib.h>  // used for rand()
 
 //============================= CPU ===========================================
@@ -18,7 +20,7 @@ This function is called by the CPU
  *  c - output array
  *  N - size of the array
  */
-void cpu_add(int *a, int *b, int *c, unsigned int N) {
+void cpu_add(float *a, float *b, float *c, int N) {
 	for (unsigned int i = 0; i < N; i++) {
 		c[i] = a[i] + b[i];
 	}
@@ -38,34 +40,24 @@ but will be called from the host. Notice that we must use pointers!
  *  b - second array
  *  c - output array
  */
-__global__ void cuda_add(int *a, int *b, int *c, unsigned int N) {
-	// init thread id
-	int tid;
-
+__global__ void cuda_add(float *a, float *b, float *c, int N) {
 	// assign tid by using block id, block dimension, and thread id
-	tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-	// stride is for big arrays, i.e. bigger than threads we have
-	int stride = blockDim.x * gridDim.x;
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
 	// do the operations
-	while (tid < N) {
+	if (tid < N) {
 		c[tid] = a[tid] + b[tid];
-		tid += stride;
 	}
 }
 
 
 int main(){
 	// size of the array
-    const int N = 1000000;
+    const int N = 1000000000;
 	printf("Array Size: %d\n", N);
 
-	// max value allowed in each array.
-	unsigned int max_value = 100;
-
 	// how many times to run it?
-	int iterations = 10000;
+	int iterations = 10;
 
 	// DEVICE PROPERTIES
 	cudaDeviceProp prop;
@@ -80,44 +72,34 @@ int main(){
 		prop.maxGridSize[1], prop.maxGridSize[2]);
 
 	// do a rough estimate of memory needed in stack
-	printf("RAM needed estimate: %lu Mbytes\n", sizeof(int)*N * 6 / 1000000);
+	printf("RAM needed estimate: %lu Mbytes\n", sizeof(float)*N * 6 / 1000000);
 
 	// allocate memory. Must use malloc() when dealing with arrays this large
 	// note this is allocating on the host computer.
-	int *a = (int *)malloc(N * sizeof(int));
-	int *b = (int *)malloc(N * sizeof(int));
-	int *c = (int *)malloc(N * sizeof(int));
-	int *d_a, *d_b, *d_c;
-
-	// set up random number generator
-	time_t tictoc;
-	srand((unsigned) time(&tictoc));
+	float *a = (float *)malloc(N * sizeof(float));
+	float *b = (float *)malloc(N * sizeof(float));
+	float *c = (float *)malloc(N * sizeof(float));
+	float *d_a, *d_b, *d_c;
 
 	// set up timer
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
-	// initialize variables for holding number of blocks and number of threads 
-	// to be launched
-	unsigned int numBlocks, numThreads;
+	// using max number of threads in the x dim possible
+	int nThreads = prop.maxThreadsDim[0];
+	printf("nThreads: %d\n", nThreads);
 
-	numThreads = prop.maxThreadsPerBlock;
-	numBlocks = (N + numThreads - 1) / numThreads;  // this rounds up to make sure
-													// enough blocks are launched
+	// calculate number of blocks based on the number of threads
+	int nBlocks = (N + nThreads - 1) / nThreads;
+	printf("nBlocks: %d\n", nBlocks);
 
-	// now need to check that we are not launching a grid larger than availible
-	if (numBlocks > prop.maxGridSize[1]) {
-		printf("Could not launch a grid of size %lu, exceeds max grid size dimension\n\
-			    of %d. Launching with %d instead.", numBlocks, prop.maxGridSize[1], 
-				prop.maxGridSize[1]);
-	}
 
 	// init the timer variable
 	float millis = 0;
 
 	// open file for keeping track of stats.
-	FILE *f = fopen("gpu_add_times.txt", "w");
+	FILE *f = fopen("gpu_add_times.txt", "a");
 
 	// error handling for file open
 	if (f == NULL)
@@ -130,48 +112,64 @@ int main(){
 	printf("Saving execution times to gpu_add_times.txt and cpu_add_times.txt\n");
 
 	// allocate memory once before the iterations
-	cudaMalloc((void **)&d_a, sizeof(int) * N);
-	cudaMalloc((void **)&d_b, sizeof(int) * N);
-	cudaMalloc((void **)&d_c, sizeof(int) * N);
+	cudaMalloc((void **)&d_a, sizeof(float) * N);
+	cudaMalloc((void **)&d_b, sizeof(float) * N);
+	cudaMalloc((void **)&d_c, sizeof(float) * N);
 	printf("Allocated memory on the Device for a, b, and c . . .\n");
 
 	float avg = 0;
 
+	// create vectors.
+	for (unsigned int i = 0; i < N; i++) {
+		// actual values don't matter, as long as they're floats.
+		a[i] = 1.0f;
+		b[i] = 2.0f;
+	}
+
+	// copy memory from CPU to GPU
+	cudaMemcpy(d_a, a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, N * sizeof(float), cudaMemcpyHostToDevice);
+
 	printf("Running on GPU\n");
 	for (int j = 0; j < iterations; j++) {
-		// create random vectors.
-		for (unsigned int i = 0; i < N; i++) {
-			a[i] = rand() % max_value;
-			b[i] = rand() % max_value;
-		}
-
-		// copy memory from CPU to GPU
-		cudaMemcpy(d_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
-		cudaMemcpy(d_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
 
 		// start timer
 		cudaEventRecord(start);
 
 		// run the kernel
-		cuda_add<<<numBlocks, numThreads>>>(d_a, d_b, d_c, N);
+		cuda_add<<<nBlocks, nThreads>>>(d_a, d_b, d_c, N);
 
 		// stop the timer
 		cudaEventRecord(stop);
 
 		// copy the result from the device back to the host
-		cudaMemcpy(c, d_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(c, d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+		// sync the events
+		cudaEventSynchronize(stop);
 
 		// calc elapsed time and print to file
 		cudaEventElapsedTime(&millis, start, stop);
-		fprintf(f, "%f \n", millis);
+		millis = millis / 1000;
+		fprintf(f, "%d,%f\n", N, millis);
 		avg = avg + millis;
-	}
 
-	avg = avg / (float) iterations;
-	printf("Average GPU Time %f: \n", avg);
+		// calculate the error.
+		float maxError = 0.0f;
+		for (int i = 0; i < N; i++) {
+			maxError = abs(c[i] - a[0] - b[0]);
+		}
+		if (maxError != 0.0) {
+			printf("Max error: %f\n", maxError);
+		}
+	}
+	
+
+	avg = (float) avg / (float) iterations;
+	printf("Average GPU Time: %fs \n", avg);
 
 	avg = 0;
-	FILE *g = fopen("cpu_add_times.txt", "w");
+	FILE *g = fopen("cpu_add_times.txt", "a");
 
 	if (g == NULL)
 	{
@@ -181,23 +179,23 @@ int main(){
 
 	printf("Running on CPU\n");
 
-	for (int j = 0; j < iterations; j++) {
-		for (unsigned int i = 0; i < N; i++) {
-			a[i] = rand() % max_value;
-			b[i] = rand() % max_value;
-		}
+	clock_t cpu_start, cpu_stop;
+	double cpu_millis;
+	double cpu_avg = 0.0;
 
-		cudaEventRecord(start); // start timer
+	for (int j = 0; j < iterations; j++) {
+		cpu_start = clock();
+
 		cpu_add(a, b, c, N); // add the vectors
-		cudaEventRecord(stop); // stop the timer
-		cudaEventSynchronize(stop); // sync
-		cudaEventElapsedTime(&millis, start, stop); // calc time
-		fprintf(g, "%f\n", millis); // print to file
-		avg = avg + millis;
+
+		cpu_stop = clock();
+		cpu_millis = ((double)(cpu_stop - cpu_start)) / CLOCKS_PER_SEC;
+		fprintf(g, "%d,%f\n", N, cpu_millis); // print to file
+		cpu_avg = avg + cpu_millis;
 	}
 
-	avg = avg / (float)iterations;
-	printf("Average CPU Time %f: \n", avg);
+	cpu_avg = cpu_avg / (double)iterations;
+	printf("Average CPU Time %f: \n", cpu_avg);
 
 	printf("Done!\n");
 	fclose(g); // close the file
--- performance_measures/plot_performance.m
+++ performance_measures/plot_performance.m
@@ -1,28 +1,15 @@
 clear all; close all;
 
-filenames_cpu = ["cpu_add_times_100_10000.txt"
-                 "cpu_add_times_1000_10000.txt"
-                 "cpu_add_times_10000_10000.txt"
-                 "cpu_add_times_100000_10000.txt"];
+filenames = ["cpu_add_times.txt","gpu_add_times.txt"];
 
-filenames_gpu = ["gpu_add_times_100_10000.txt"
-                 "gpu_add_times_1000_10000.txt"
-                 "gpu_add_times_10000_10000.txt"
-                 "gpu_add_times_100000_10000.txt"];
-
-plot_perf(filenames_cpu);
-plot_perf(filenames_gpu);
-
-function fig = plot_perf(filenames)
-    % read in the file
-    fig = figure;
-    for i=1:length(filenames)
-        disp(filenames(i))
-        f = fopen(filenames(i));
-        data = fscanf(f, '%f');
-        fclose(f);
-        plot(data,'o','color',rand(1,3));
-        hold on;
-    end
+% read in the file
+fig = figure;
+for i=1:length(filenames)
+    disp(filenames(i))
+    f = fopen(filenames(i));
+    data = csvread(filenames(i));
+    loglog(data(:,1),data(:,2),'*-');
+    hold on;
+    grid on;
 end
 
