f60b048c45e40a07cd5861665f61e51b85252e3f
Matthew Hanley <mattdhanley@gmail.com>
2018-10-31 18:33:50 +0000

    tk1

--- performance_measures/easy_nvcc.sh
+++ performance_measures/easy_nvcc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvcc adding_cpu.cu common.cpp multiply_gpu.cu transpose_gpu.cu adding_gpu.cu theoretical_bandwidth.cu main.cu -o performance_test -arch=sm_61
+nvcc adding_cpu.cu common.cpp multiply_gpu.cu transpose_gpu.cu adding_gpu.cu theoretical_bandwidth.cu main.cu -o performance_test -arch=sm_32
